65755e4259a86b753320ee2a0a3a2014f8836bd6
DESKTOP-Q31DD3F\LENOVO <mansouriii.hanaa@gmail.com>
2025-03-01 20:27:32 +0000

    lombok logging

--- src/main/java/hm/springframework/spring6restmvc/controllers/BeverageController.java
+++ src/main/java/hm/springframework/spring6restmvc/controllers/BeverageController.java
@@ -1,12 +1,25 @@
 package hm.springframework.spring6restmvc.controllers;
 
+import hm.springframework.spring6restmvc.model.Beverage;
 import hm.springframework.spring6restmvc.services.BeverageService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+
+import java.util.UUID;
 
 @AllArgsConstructor
 @Controller
 public class BeverageController {
 
+    private static final Logger log = LoggerFactory.getLogger(BeverageController.class);
     private final BeverageService beverageService;
+
+    public Beverage getBeverageById(UUID id){
+
+        log.debug("Get Beverage by Id - in controller");
+        return beverageService.getBeerById(id);
+    }
+
 }
--- src/main/java/hm/springframework/spring6restmvc/services/BeverageServiceImpl.java
+++ src/main/java/hm/springframework/spring6restmvc/services/BeverageServiceImpl.java
@@ -2,16 +2,19 @@ package hm.springframework.spring6restmvc.services;
 
 import hm.springframework.spring6restmvc.model.Beverage;
 import hm.springframework.spring6restmvc.model.BeverageStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeverageServiceImpl implements BeverageService {
     @Override
     public Beverage getBeerById(UUID id) {
+        log.debug("Get Beverage by Id - in service. Id: " + id.toString());
         return Beverage.builder()
                 .id(id)
                 .version(1)
