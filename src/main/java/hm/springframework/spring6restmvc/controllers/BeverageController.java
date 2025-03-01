0f1d9a47abb6ef18c4f36ed9ea81f5faa717ce90
DESKTOP-Q31DD3F\LENOVO <mansouriii.hanaa@gmail.com>
2025-03-01 22:13:19 +0000

    HTTP client

--- src/main/java/hm/springframework/spring6restmvc/controllers/BeverageController.java
+++ src/main/java/hm/springframework/spring6restmvc/controllers/BeverageController.java
@@ -5,12 +5,16 @@ import hm.springframework.spring6restmvc.services.BeverageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
+
 @RestController
+@RequestMapping("/api/v1/beverages")
 public class BeverageController {
 
     private static final Logger log = LoggerFactory.getLogger(BeverageController.class);
@@ -20,13 +24,14 @@ public class BeverageController {
         this.beverageService = beverageService;
     }
 
-    public Beverage getBeverageById(UUID id) {
+    @GetMapping("/{beverageId}")
+    public Beverage getBeverageById(@PathVariable("beverageId") UUID beverageId) {
 
         log.debug("Get Beverage by Id - in controller");
-        return beverageService.getBeerById(id);
+        return beverageService.getBeerById(beverageId);
     }
 
-    @GetMapping("api/v1/beverages")
+    @GetMapping()
     public List<Beverage> listBeverages() {
         return beverageService.listBeverages();
     }
