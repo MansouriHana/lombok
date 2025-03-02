2bda85e316a81dea919fc73671a1d4e4e5a55985
DESKTOP-Q31DD3F\LENOVO <mansouriii.hanaa@gmail.com>
2025-03-02 15:06:28 +0000

    mockMvc-configuration

--- src/test/java/hm/springframework/spring6restmvc/controllers/BeverageControllerTest.java
+++ src/test/java/hm/springframework/spring6restmvc/controllers/BeverageControllerTest.java
@@ -1,21 +1,32 @@
 package hm.springframework.spring6restmvc.controllers;
 
+import hm.springframework.spring6restmvc.services.BeverageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest(BeverageController.class)
 class BeverageControllerTest {
 
     @Autowired
-    BeverageController beverageController;
+    MockMvc mockMvc;
+
+    @MockitoBean
+    BeverageService beverageService;
 
     @Test
-    void getBeverageById() {
-        System.out.println(beverageController.getBeverageById(UUID.randomUUID()));
+    void getBeverageById() throws Exception {
+        mockMvc.perform(get("/api/v1/beverages" + UUID.randomUUID())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
     }
 }
