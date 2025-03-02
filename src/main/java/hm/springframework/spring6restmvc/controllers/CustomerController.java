12cd9b3b46782fc0f36e1c91d6cc007d94bdd428
DESKTOP-Q31DD3F\LENOVO <mansouriii.hanaa@gmail.com>
2025-03-02 12:58:36 +0000

    HTTP DELETE  with spring MVC

--- src/main/java/hm/springframework/spring6restmvc/controllers/CustomerController.java
+++ src/main/java/hm/springframework/spring6restmvc/controllers/CustomerController.java
@@ -38,4 +38,11 @@ public class CustomerController {
         headers.add("Location", "/api/v1/customer/" + savedCustomer.getId().toString());
         return new ResponseEntity<>(savedCustomer, headers, HttpStatus.CREATED);
     }
+
+    @PutMapping("{customerId}")
+    public ResponseEntity<Customer> handlePutCustomer(@PathVariable("customerId") UUID customerId, @RequestBody Customer customer) {
+
+        customerService.updateCustomerById(customerId, customer);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/hm/springframework/spring6restmvc/services/CustomerService.java
+++ src/main/java/hm/springframework/spring6restmvc/services/CustomerService.java
@@ -11,4 +11,6 @@ public interface CustomerService {
     public Customer findCustomerById(UUID id);
 
     Customer saveNewCustomer(Customer customer);
+
+    void updateCustomerById(UUID customerId, Customer customer);
 }
